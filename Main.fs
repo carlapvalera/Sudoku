4ac84d6b8ebdb3746165dca65c5e2613eda436af
carlapvalera <perezvaleracarlas@gmail.com>
2023-06-13 23:22:07 -0400

    primeros pasos

---
+++ Main.fs
@@ -0,0 +1,14 @@
+﻿open Sudo
+open System
+
+let main = Console.WriteLine(Row(0,5))
+Console.WriteLine(5%3) 
+
+for i in 0 .. 8 do
+    for j in 0 .. 7 do
+        Console.Write("{0}  ",Sudo[i,j])
+    Console.Write("{0} \n",Sudo[i,8])
+
+
+
+
---
+++ Sudo.fs
@@ -0,0 +1,22 @@
+﻿module Sudo
+    //type Box ={row : int; col : int; value: int}
+
+    let arrayOfArrays = [| [| 5;3;0;0;7;0;0;0;0|]; [|6;0;0;1;9;5;0;0;0|];[|0;9;8;0;0;0;0;6;0|];[|8;0;0;0;6;0;0;0;3|];[|4;0;0;8;0;3;0;0;1|];[|7;0;0;0;2;0;0;0;6|];[|0;6;0;0;0;0;2;8;0|];[|0;0;0;4;1;9;0;0;5|];[|0;0;0;0;8;0;0;7;9|]|]
+    let Sudo = Array2D.init 9 9 (fun i j -> arrayOfArrays[i][j])
+
+  
+    let Row(row : int, number :int)=
+        arrayOfArrays[row]
+        |> Array.contains(number)
+
+    let Column(column : int, number :int)=
+        arrayOfArrays[column]
+        |> Array.contains(number)
+
+    //let Box(row : int,column : int, number :int)
+        
+
+
+      
+
+
