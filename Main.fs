7a04b12905ec461b41c610c9cecec11227883193
carlapvalera <perezvaleracarlas@gmail.com>
2023-06-16 09:51:33 -0400

    probar el 3x3 y ecrear recurs

--- Main.fs
+++ Main.fs
@@ -1,12 +1,16 @@
 ﻿open Sudo
 open System
 
-let a = possible_number_positions
+//let a = blabla()
 let main = 
-    for i in 0..80 do 
-    Console.WriteLine(possible_number_positions[0])
-Console.WriteLine(Box(0,5))
-Console.WriteLine(5%3) 
+    //let b =arrayOfArrays_2
+    let a = boxing(0,0)
+    for i in 0..2 do 
+        for j in 0..2 do
+ //       Console.WriteLine("\n")
+
+//Console.WriteLine(possible_number(0,0)
+//Console.WriteLine(5%3) 
 
 for i in 0 .. 8 do
     for j in 0 .. 7 do
